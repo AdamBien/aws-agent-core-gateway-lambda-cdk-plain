f71be8cde27cf26f4db98b648826458743e13b30
Adam Bien <abien@adam-bien.com>
2025-11-07 21:33:47 +0100

    lambda creation control

---
+++ cdk/src/main/java/airhacks/qacg/lambda/control/Functions.java
@@ -0,0 +1,26 @@
+package airhacks.qacg.lambda.control;
+
+import java.util.Map;
+
+import software.amazon.awscdk.Duration;
+import software.amazon.awscdk.services.lambda.Architecture;
+import software.amazon.awscdk.services.lambda.Code;
+import software.amazon.awscdk.services.lambda.Function;
+import software.amazon.awscdk.services.lambda.Runtime;
+import software.constructs.Construct;
+
+public interface Functions{
+
+    static Function createFunction(Construct app,String functionName,String functionHandler, Map<String,String> configuration, int memory, int timeout) {
+        return Function.Builder.create(app, functionName)
+                .runtime(Runtime.JAVA_21)
+                .architecture(Architecture.ARM_64)
+                .code(Code.fromAsset("../lambda/target/function.jar"))
+                .handler(functionHandler)
+                .memorySize(memory)
+                .functionName(functionName)
+                .environment(configuration)
+                .timeout(Duration.seconds(timeout))
+                .build();
+    }
+}
