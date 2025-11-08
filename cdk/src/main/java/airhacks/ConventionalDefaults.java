3347fe3a98fc73aa5d345748e374cd0d431e3d65
Adam Bien <abien@adam-bien.com>
2025-11-08 19:19:55 +0100

    Lambda conventions added

--- cdk/src/main/java/airhacks/ConventionalDefaults.java
+++ cdk/src/main/java/airhacks/ConventionalDefaults.java
@@ -1,6 +1,8 @@
 package airhacks;
 
 public interface ConventionalDefaults {
+    int memory = 1769; 
+    int timeout = 30;
 
     static String stackName(String appName, String stackName) {
         return "%s-%s-stack".formatted(appName, stackName);
--- cdk/src/main/java/airhacks/qacg/lambda/control/Functions.java
+++ cdk/src/main/java/airhacks/qacg/lambda/control/Functions.java
@@ -2,6 +2,7 @@ package airhacks.qacg.lambda.control;
 
 import java.util.Map;
 
+import airhacks.ConventionalDefaults;
 import software.amazon.awscdk.Duration;
 import software.amazon.awscdk.services.lambda.Architecture;
 import software.amazon.awscdk.services.lambda.Code;
@@ -10,6 +11,11 @@ import software.amazon.awscdk.services.lambda.Runtime;
 import software.constructs.Construct;
 
 public interface Functions{
+
+    static Function createFunction(Construct app,String functionName,String functionHandler){
+        return createFunction(app, functionName, functionHandler,Map.of(),ConventionalDefaults.memory,ConventionalDefaults.timeout);
+
+    }
 
     static Function createFunction(Construct app,String functionName,String functionHandler, Map<String,String> configuration, int memory, int timeout) {
         return Function.Builder.create(app, functionName)
