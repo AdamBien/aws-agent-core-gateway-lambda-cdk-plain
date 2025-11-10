f499541c9a66c7af7caa4012c84e53a7d0e1272a
Adam Bien <abien@adam-bien.com>
2025-11-10 08:53:25 +0100

    user pool reconfigured

--- cdk/src/main/java/airhacks/qacg/cognito/control/UserPools.java
+++ cdk/src/main/java/airhacks/qacg/cognito/control/UserPools.java
@@ -2,37 +2,51 @@ package airhacks.qacg.cognito.control;
 
 import software.amazon.awscdk.services.cognito.UserPool;
 import software.amazon.awscdk.services.cognito.UserPoolClient;
+import software.amazon.awscdk.RemovalPolicy;
+import software.amazon.awscdk.services.cognito.AuthFlow;
+import software.amazon.awscdk.services.cognito.Mfa;
 import software.amazon.awscdk.services.cognito.OAuthFlows;
 import software.amazon.awscdk.services.cognito.OAuthSettings;
+import software.amazon.awscdk.services.cognito.PasswordPolicy;
+import software.amazon.awscdk.services.cognito.SignInAliases;
 import software.constructs.Construct;
 
 public interface UserPools {
 
-    static UserPool create(Construct scope) {
-        return UserPool.Builder.create(scope, "UserPool")
-                .userPoolName("agent-core-gateway-users")
-                .selfSignUpEnabled(true)
-                .signInAliases(software.amazon.awscdk.services.cognito.SignInAliases.builder()
-                        .email(true)
-                        .build())
-                .build();
-    }
+        static UserPool create(Construct scope) {
+                return UserPool.Builder.create(scope, "UserPool")
+                                .userPoolName("agent-core-gateway-users")
+                                .selfSignUpEnabled(true)
+                                .signInAliases(SignInAliases.builder()
+                                                .email(true)
+                                                .build())
+                                .passwordPolicy(PasswordPolicy.builder()
+                                                .minLength(8)
+                                                .requireLowercase(true)
+                                                .requireUppercase(true)
+                                                .requireDigits(false)
+                                                .requireSymbols(false)
+                                                .build())
+                                .mfa(Mfa.OPTIONAL)
+                                .removalPolicy(RemovalPolicy.DESTROY)
 
-    static UserPoolClient createClient(Construct scope, UserPool userPool) {
-        return UserPoolClient.Builder.create(scope, "UserPoolClient")
-                .userPoolClientName("agent-core-gateway-client")
-                .userPool(userPool)
-                .generateSecret(false)
-                .authFlows(software.amazon.awscdk.services.cognito.AuthFlow.builder()
-                        .userPassword(true)
-                        .userSrp(true)
-                        .build())
-                .oAuth(OAuthSettings.builder()
-                        .flows(OAuthFlows.builder()
-                                .implicitCodeGrant(true)
-                                .build())
-                        .build())
-                .build();
-    }
+                                .build();
+        }
+
+        static UserPoolClient createClient(Construct scope, UserPool userPool) {
+                return UserPoolClient.Builder.create(scope, "UserPoolClient")
+                                .userPoolClientName("agent-core-gateway-client")
+                                .userPool(userPool)
+                                .generateSecret(false)
+                                .authFlows(AuthFlow.builder()
+                                                .userPassword(true)
+                                                .build())
+                                .oAuth(OAuthSettings.builder()
+                                                .flows(OAuthFlows.builder()
+                                                                .implicitCodeGrant(true)
+                                                                .build())
+                                                .build())
+                                .build();
+        }
 
 }
